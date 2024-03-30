08fee2629fbc52918ceaeb89a8878c5a64c70dab
Betül Sağlam <betul.saglam004@gmail.com>
2024-03-30 23:47:33 +0300

    Compute.java coverage increased to 100%

--- my-app/src/test/java/TestCompute.java
+++ my-app/src/test/java/TestCompute.java
@@ -34,4 +34,29 @@ public class TestCompute {
     c = new Compute(mq);
     assertEquals(0, c.countNumberOfOccurrences("Hello World!"));
   }
+
+  @Test
+  public void testNotEquals(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(5);
+    when(mq.contains(anyString())).thenReturn(true);
+    when(mq.getAt(anyInt())).thenReturn("x");
+
+    c = new Compute(mq);
+    assertEquals(0, c.countNumberOfOccurrences("im tired"));
+  }
+
+  @Test
+  public void testCounter(){
+    MessageQueue mq = mock(MessageQueue.class);
+    when(mq.size()).thenReturn(3);
+    when(mq.contains(anyString())).thenReturn(true);
+
+    when(mq.getAt(0)).thenReturn("i should study");
+    when(mq.getAt(1)).thenReturn("but i will");
+    when(mq.getAt(2)).thenReturn("sleep");
+
+    c = new Compute(mq);
+    assertEquals(1, c.countNumberOfOccurrences("sleep"));
+  }
 }
